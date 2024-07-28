d84881a977473d59950a31ca641ea4597e0e9281
monilprajapati <monillprajapatii@gmail.com>
2024-07-28 13:00:34 +0530

    Added artifacts

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}', // Adjust if your specs are in a different location
-    video: false,
+    video: true,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
